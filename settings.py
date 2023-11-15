d777d857e7fdc855273c190f39a562e1cdcfab74
Cason Nichols <casonnichols@gmail.com>
2023-11-14 20:52:09 -0500

    Changed music selection
    Changed music used for the start menu.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ logo_image_path = "CovertCombatLogo.png"
 start_instructions = ["You and the other player control two tanks amongst a sea of tanks.",
                        "It is your job to hunt and kill the other player, without being killed.",
                        "Press SPACE to start, or ESC to quit."]
-start_music_path = 'BF1942 Music/05 - Theme 2.mp3'
+start_music_path = 'BF1942 Music/06 - Vehicle 3.mp3'
 start_font_path = 'Capsmall.ttf'
 
 #Tank Settings
