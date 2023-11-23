27d4bf3827baac739b8d366097470e25fc1c131b
Cason Nichols <casonnichols@gmail.com>
2023-11-23 12:21:48 -0500

    Began work on NPC pathfinding by outlining the system.

--- NPC.py
+++ NPC.py
@@ -11,13 +11,31 @@ class NPC(BaseTank):
 
         #Initialialize tank properties.
         super().__init__(game, game.NPC_group, startPosition, startAngle)
+        self.destination = (0,0)
+        self.destinationAngle = 0
         self.add(game.NPC_group)
 
-    def get_movement(self): #Generate movement for the NPC. So far, it does nothing.
+    def get_movement(self): #Generate movement for the NPC.
         pass
+
+        #Rotating the tank towards that new vector
+
+        #Increasing the tank's speed
+
+        #Random turret movement.
+
+    def generateDestination(self): #Generates a viable destination for the NPC.
+        currentPos = self.map_pos
+
+        if currentPos[0] > 0: #If the player is not on the top layer,
+            if(self.game.map.mini_map[currentPos[0] - 1][currentPos[1]] == False): #Check if space above is empty
+                pass
 
     # Override of the BaseTank method. So far it does nothing, but any NPC specific logic can be written here.
     def update(self):
+        if self.map_pos == self.destination:
+            self.generateDestination()
+
         self.get_movement()
         
         super().update()
